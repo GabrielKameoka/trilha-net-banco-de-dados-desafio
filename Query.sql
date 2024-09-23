fea2619579babdf66d6e195f2dc434ba7a21f0a8
GabrielKameoka <gabrielkameoka@gmail.com>
2024-09-23 11:10:40 -0300

    Desafio finalizado. Tempo: 2h 30m

--- Query.sql
+++ Query.sql
@@ -19,3 +19,36 @@ WHERE Ano > 2000
 ORDER BY Ano
 
 --Objetivo 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT * FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC
+
+--Objetivo 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+SELECT Ano, COUNT(Ano) AS Quantidade FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--Objetivo 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+--Objetivo 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT * FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome 
+
+--Objetivo 10 - Buscar o nome do filme e o gênero
+SELECT Nome, Genero FROM FIlmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+--Objetivo 11 - Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT Nome, Genero FROM FIlmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+--Objetivo 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT Nome, PrimeiroNome, UltimoNome, Papel FROM Filmes
+INNER JOIN ElencoFilme ON ElencoFilme.IdFilme = Filmes.Id
+INNER JOIN Atores ON ElencoFilme.IdAtor = Atores.Id
